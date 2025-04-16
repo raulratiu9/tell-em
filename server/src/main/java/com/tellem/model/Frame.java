2b0f27672f4a59e29ccd2b0da0cb1cbfaba7f67f
Raul Ratiu <raulratiu2@gmail.com>
2025-04-16 12:58:45 +0300

    [MONGO-203] fix dependencies

--- server/src/main/java/com/tellem/model/Frame.java
+++ server/src/main/java/com/tellem/model/Frame.java
@@ -1,5 +1,6 @@
 package com.tellem.model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Document(collection = "frames")
+@Data
 public class Frame {
 
     @Id
@@ -18,47 +20,8 @@ public class Frame {
     private String storyId;
 
     @DBRef
-    private List<Choice> choices = new ArrayList<>(); // Initialize the choices list
+    private List<Choice> choices = new ArrayList<>();
 
     @DBRef
-    private Story story;  // Reference to the story this frame belongs to
-
-    // Getter and Setter methods
-    public List<Choice> getChoices() {
-        return choices;
-    }
-
-    public void setChoices(List<Choice> choices) {
-        this.choices = choices;
-    }
-
-    public void addChoice(Choice choice) {
-        if (choices == null) {
-            choices = new ArrayList<>();
-        }
-        choices.add(choice);
-    }
-
-    public void setStoryId(String storyId) {
-        this.storyId = storyId;
-    }
-
-    public String getFrameKey() {
-        return frameKey;
-    }
-
-    public void setFrameKey(String frameKey) {
-        this.frameKey = frameKey;
-
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setStory(Story savedStory) {
-        this.story = savedStory;
-    }
-
-    // Other getter/setter methods...
+    private Story story;
 }
